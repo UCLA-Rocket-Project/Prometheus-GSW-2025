00e195907293c3216b83c34b8f27046faebbd4fa
SleepyWoodpecker <shawnweichew@gmail.com>
2025-02-13 23:09:03 -0800

    *should* be able to write to the SD card
    Works in theory but idk about reality

--- DAQ/DAQtransmitter/DAQtransmitter.ino
+++ DAQ/DAQtransmitter/DAQtransmitter.ino
@@ -4,6 +4,8 @@
 #include <Adafruit_ADS1X15.h>
 #include <HardwareSerial.h>
 #include "SPI.h"
+#include "FS.h"
+#include "SD.h"
 using namespace std;
 
 //Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
@@ -14,10 +16,20 @@ const int NUM_PT = 3;
 unsigned long long delay_time = 200;
 unsigned long long last_time = 0;
 
+
 // ETHERNET CONNECTION
 #define RO_PIN 16
 #define DI_PIN 17
 #define DE_RE_PIN 23
+
+// microSD card connection
+#define SD_SCK 18 // connect this to CLK pin
+#define SD_MISO 19 // connect this to DO pin
+#define SD_MOSI 23 // connect this to the DI pin
+#define SD_CS 5 // connect this to the CS pin
+
+const int FILE_NAME_MAX_LENGTH = 50;
+char new_file_name[FILE_NAME_MAX_LENGTH + 1];
 
 HardwareSerial rs485Serial(2);
 
@@ -44,13 +56,37 @@ void setup() {
   // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
   // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
   // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
-  if (!ads.begin()) {
-    Serial.println("Failed to initialize ADS.");
-    while (1);
+  // if (!ads.begin()) {
+  //   Serial.println("Failed to initialize ADS.");
+  //   while (1);
+  // }
+
+  SPI.begin(SD_SCK, SD_MISO, SD_MOSI, SD_CS);
+    
+  // Start SD card
+  if(!SD.begin(SD_CS, SPI, 4000000)){ // 4MHz SPI speed
+      Serial.println("Card Mount Failed");
+      return;
   }
+
+  uint8_t cardType = SD.cardType();
+  if(cardType == CARD_NONE){
+      Serial.println("No SD card attached");
+      return;
+  }
+
+  Serial.println("SD Card initialized.");
+  getNewFilename(new_file_name);
+  
+  Serial.print("The new file's name is: ");
+  Serial.println(new_file_name);
+  writeFile(SD, new_file_name, "pt1,pt2,pt3");
 }
+
 void loop() {
   if((millis() - last_time) > delay_time){
+    std::string string_to_write;
+
     float ptVals[NUM_PT];
 
     // Read Pressure Transducer values
@@ -58,6 +94,9 @@ void loop() {
     {
       float raw = ads.readADC_SingleEnded(i);
       ptVals[i] = ads.computeVolts(raw);
+
+      string_to_write += std::to_string(raw);
+      string_to_write += i < NUM_PT - 1 ? "," : "";
     }
     //  // Calibration for PTs (likely have to calibrate everytime you flow)
     ptVals[0] = ptVals[0] * 381.0376825 - 124.3444374;
@@ -84,9 +123,79 @@ void loop() {
     digitalWrite(DE_RE_PIN, HIGH);
     delay(50);
     rs485Serial.println(storeStr);
+
+    // set up the string for writing
+    string_to_write += '\n';
+    Serial.print("Writing: ");
+    Serial.println(string_to_write.c_str());
+    appendFile(SD, new_file_name, string_to_write.c_str());
   }
 }
 
+bool getNewFilename(char* new_file_name) {
+  int counter = 0;
+  char candidate_file_name[FILE_NAME_MAX_LENGTH + 1];
+  while (true) {
+    sprintf(candidate_file_name, "/data%d.csv", counter);
+    Serial.print("Checking file: ");
+    Serial.println(candidate_file_name);
+    if (!SD.exists(candidate_file_name)) {
+      sprintf(new_file_name, candidate_file_name);
+      return true;
+    }
+    ++counter;
+  }
+
+  return false;
+}
+
+void readFile(fs::FS &fs, const char * path){
+  Serial.printf("Reading file: %s\n", path);
+
+  File file = fs.open(path);
+  if(!file){
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  Serial.print("Read from file: ");
+  while(file.available()){
+    Serial.write(file.read());
+  }
+  file.close();
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+  Serial.printf("Writing file: %s\n", path);
+
+  File file = fs.open(path, FILE_WRITE);
+  if(!file){
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if(file.print(message)){
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+  file.close();
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if(!file){
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if(file.print(message)){
+      Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
+}
 
 
 
