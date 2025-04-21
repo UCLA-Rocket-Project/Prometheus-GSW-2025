5dafe0b77daca75507e2ffe08ad1d9e7479b1cc3
euanlimzx <euanlimzx@gmail.com>
2025-04-21 15:46:06 -0700

    idk if any of these work, pushing for now

---
+++ ryansnewboard/ares/main.cpp
@@ -0,0 +1,79 @@
+// ARES EGSE 2024-2025 Load Cell System Code
+// ESP32-S3 Documentation: https://www.espressif.com/sites/default/files/documentation/esp32-s3_technical_reference_manual_en.pdf
+// ADS1256 Documentation: https://www.ti.com/lit/gpn/ads1256
+// ADS1256 Library Information: https://github.com/CuriousScientist0/ADS1256/tree/main
+
+#include <Arduino.h>
+#include <ADS1256.h>
+
+// Define custom SPI pins for ADS1256 Module
+#define SPI_MISO 37
+#define SPI_SCLK 36
+#define SPI_MOSI 35
+#define SPI_CS  34
+
+#define DRDY 38
+
+// Define LED indicator pin
+#define LED 7
+
+SPIClass spi_ads(FSPI); // Create custom SPI instance
+ADS1256 ADC(&spi_ads, DRDY, SPI_CS, 2.5); // Instantiate ADS1256
+
+// Calibration coefficients y=Ax+B
+float calibrationA = -195.25664;
+float calibrationB = -91.19858;
+
+// convertToWeight function
+// Returns measured force (lbf)
+float convertToWeight(float voltage) {
+  return (calibrationA * voltage) + calibrationB;
+}
+
+void setup()
+{
+    // Initialize Serial
+    Serial.begin(115200);
+    delay(100);
+
+    // Configure SPI pins
+    pinMode(SPI_MISO, INPUT_PULLUP);
+    pinMode(SPI_SCLK, OUTPUT);
+    pinMode(SPI_MOSI, OUTPUT);
+
+    // Configure LED pin and write HIGH
+    pinMode(LED, OUTPUT); digitalWrite(LED, HIGH);
+
+    // Initialize the SPI bus with custom pins
+    spi_ads.begin(SPI_SCLK, SPI_MISO, SPI_MOSI, SPI_CS);
+
+    // ADC Setup
+    ADC.InitializeADC(); 
+
+    // Set Gain (PGA)
+    ADC.setPGA(PGA_64);  // Maximum gain for small signals
+
+    // Set Differential Input Channels (AIN0 - AIN1)
+    ADC.setMUX(DIFF_0_1);  
+
+    // Set Sampling Rate
+    ADC.setDRATE(DRATE_1000SPS);  
+
+    delay(100);
+}
+
+void loop()
+{
+  // Read differential voltage
+  long rawConversion = ADC.readSingleContinuous();    
+
+  // Convert raw ADC value to voltage
+  float voltageValue = ADC.convertToVoltage(rawConversion) * 1000; 
+
+  // Convert voltage to weight
+  float weight = convertToWeight(voltageValue);      
+
+  // Print results
+  Serial.print(String(weight,2));
+  Serial.println();
+}
---
+++ ryansnewboard/pt/pt.ino
@@ -0,0 +1,49 @@
+// this is the code for the PT, take note that you multiply the calibrated voltage by 0.5 because of there was some inconsistency with the original datasheet
+
+#include <Arduino.h>
+#include "ADS8688.h"
+
+ADS8688 adc;
+
+#define MISO 35
+#define SCLK 48
+#define MOSI 34
+#define CS_2 36
+
+void setup() {
+    // Initialize Serial
+    Serial.begin(115200);
+
+    delay(5);
+
+    Serial.println("Ready!");
+    
+    // Initialize ADCs
+    // NOTE: Change pins later, change back to 0x05 and correct if reading is not correct
+    adc.begin(MISO, SCLK, MOSI, CS_2, 4.1, 0x05);
+    adc.setInputRange(CS_2, 0x05);
+
+    Serial.println("End Setup");
+}
+
+void loop() {
+    Serial.println("HI");
+    float voltages[8]; // Contains 8 Pressure Readings
+    float calibratedVoltages[8]; // Store calibrated values
+
+    // Read PTs from ADC (CS39)
+    adc.readAllChannels(CS_2, true, voltages);
+
+    // Apply calibration to PTs
+    for (int i = 0; i < 8; i++) {
+        calibratedVoltages[i] = 0.5f * voltages[i];
+        // calibratedVoltages[i] = voltages[i];
+    }
+
+    // Print all 8 values in one line
+    for (int i = 0; i < 7; i++) {
+        Serial.print(String(calibratedVoltages[i],2) + ",");
+    }
+    Serial.println(String(calibratedVoltages[7],2)); // Last value without trailing comma
+    delay(100);
+}
