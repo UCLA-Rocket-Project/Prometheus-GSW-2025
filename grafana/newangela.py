db86db41000f705f731d5629b634aba682b8d98e
ayxz0 <angelaz.yuxi@gmail.com>
2025-05-08 21:12:52 -0700

    @euan

--- grafana/newangela.py
+++ grafana/newangela.py
@@ -3,10 +3,11 @@ import sys
 import time
 import socket
 import os.path
+import json
 import paho.mqtt.client as paho
 import socketio
 
-WEBSOCKET_ADDRESS = "http://localhost:3000"
+WEBSOCKET_ADDRESS = "http://localhost:3001/"
 LIVE_DATA_PUSH_CHANNEL = "live/receive-data-stream-from-mqtt"
 sio = socketio.Client()
 PORT = 'COM5'  # windows
@@ -24,14 +25,26 @@ while os.path.exists(path):
     path = fileName+'_'+str(fileCt)+'.csv'
     fileCt += 1
 
-def format_data_for_websockets(sensor_data):
+def convert_to_dict(arr):
+    result = {}
+    for item in arr:
+        key, value = item.split('=')
+        try:
+            result[key] = float(value)
+        except ValueError:
+            result[key] = value
+    return result
+
+
+def format_data_for_websockets(sensor_string):
+        sensor_data = convert_to_dict(sensor_string)
         hud_config = [
         {
             'dataName': 'Pressure Transducer 1',
             'dataCol': 1,
             'min': -1000.0,
             'max': 1000.0,
-            'value': sensor_data['pt1'],
+            'value': 500,
             'secondDisplayType': 'graph',
             'units': '°',
         },
@@ -40,7 +53,7 @@ def format_data_for_websockets(sensor_data):
     'dataCol': 1,
     'min': -1000.0,
     'max': 1000.0,
-    'value': sensor_data['pt2'],
+    'value': 500,
     'secondDisplayType': 'graph',
     'units': '°',
 },
@@ -49,7 +62,7 @@ def format_data_for_websockets(sensor_data):
     'dataCol': 1,
     'min': -1000.0,
     'max': 1000.0,
-    'value': sensor_data['pt3'],
+    'value': 500,
     'secondDisplayType': 'graph',
     'units': '°',
 },
@@ -58,7 +71,7 @@ def format_data_for_websockets(sensor_data):
     'dataCol': 1,
     'min': -1000.0,
     'max': 1000.0,
-    'value': sensor_data['pt4'],
+    'value': 500,
     'secondDisplayType': 'graph',
     'units': '°',
 },
@@ -67,7 +80,7 @@ def format_data_for_websockets(sensor_data):
     'dataCol': 1,
     'min': -1000.0,
     'max': 1000.0,
-    'value': sensor_data['pt5'],
+    'value': 500,
     'secondDisplayType': 'graph',
     'units': '°',
 },
@@ -76,7 +89,7 @@ def format_data_for_websockets(sensor_data):
     'dataCol': 1,
     'min': -1000.0,
     'max': 1000.0,
-    'value': sensor_data['pt6'],
+    'value': 500,
     'secondDisplayType': 'graph',
     'units': '°',
 },
@@ -85,7 +98,7 @@ def format_data_for_websockets(sensor_data):
     'dataCol': 1,
     'min': -1000.0,
     'max': 1000.0,
-    'value': sensor_data['lc1'],
+    'value': 500,
     'secondDisplayType': 'graph',
     'units': '°',
 },
@@ -94,11 +107,12 @@ def format_data_for_websockets(sensor_data):
     'dataCol': 1,
     'min': -1000.0,
     'max': 1000.0,
-    'value': sensor_data['lc2'],
+    'value': 500,
     'secondDisplayType': 'graph',
     'units': '°',
 },
         ]
+        return hud_config
 
 def send_to_websocket(data, channel=LIVE_DATA_PUSH_CHANNEL):
     try:
@@ -132,60 +146,143 @@ def writeToCSV(pt_data, writer):
 def main():
     global flag
     flag = True
+    try:
+        sio.connect(WEBSOCKET_ADDRESS, wait_timeout=10)
+    except Exception as e:
+        print("WebSocket Connection Error:", e)
+        return
+    while True:
+        time.sleep(1)
+        send_to_websocket(json.dumps([
+        {
+            'dataName': 'Pressure Transducer 1',
+            'dataCol': 1,
+            'min': -1000.0,
+            'max': 1000.0,
+            'value': 500,
+            'secondDisplayType': 'graph',
+            'units': '°',
+        },
+        {
+    'dataName': 'Pressure Transducer 2',
+    'dataCol': 1,
+    'min': -1000.0,
+    'max': 1000.0,
+    'value': 500,
+    'secondDisplayType': 'graph',
+    'units': '°',
+},
+        {
+    'dataName': 'Pressure Transducer 3',
+    'dataCol': 1,
+    'min': -1000.0,
+    'max': 1000.0,
+    'value': 500,
+    'secondDisplayType': 'graph',
+    'units': '°',
+},
+        {
+    'dataName': 'Pressure Transducer 4',
+    'dataCol': 1,
+    'min': -1000.0,
+    'max': 1000.0,
+    'value': 500,
+    'secondDisplayType': 'graph',
+    'units': '°',
+},
+        {
+    'dataName': 'Pressure Transducer 5',
+    'dataCol': 1,
+    'min': -1000.0,
+    'max': 1000.0,
+    'value': 500,
+    'secondDisplayType': 'graph',
+    'units': '°',
+},
+        {
+    'dataName': 'Pressure Transducer 6',
+    'dataCol': 1,
+    'min': -1000.0,
+    'max': 1000.0,
+    'value': 500,
+    'secondDisplayType': 'graph',
+    'units': '°',
+},
+ {
+    'dataName': 'Load Cell 1',
+    'dataCol': 1,
+    'min': -1000.0,
+    'max': 1000.0,
+    'value': 500,
+    'secondDisplayType': 'graph',
+    'units': '°',
+},
+ {
+    'dataName': 'Load Cell 2',
+    'dataCol': 1,
+    'min': -1000.0,
+    'max': 1000.0,
+    'value': 500,
+    'secondDisplayType': 'graph',
+    'units': '°',
+}]
+        ))
     # Open the CSV file and set up the writer
-    with open(path, mode='a', newline='') as file:  # Open the correct file here
-        writer = csv.writer(file)
-        # Write the header if the file is empty
-        if file.tell() == 0:
-            writer.writerow(['pt1', 'pt2', 'pt3', 'pt4', 'pt5', 'pt6', 'pt7', 'pt8', 'lc1', 'lc2', 'timestamp'])
-        def parseInputAndGrafana(input):
-            global flag
-            # Read the data and stream
-            if flag:
-                try:
-                    pt_data = input
-                except KeyboardInterrupt:
-                    # Handle KeyboardInterrupt to gracefully exit the program
-                    print(f"\nProgram terminated by user. Wrote data to {path}\n")
-                    flag = False
-                else:
-                    if pt_data.startswith('A') and pt_data.endswith('Z'):
-                        # Remove the 'A' at the start and 'Z' at the end
-                        pt_data = pt_data[1:-1]
-                        # Add the timestamp at the end (you can modify this based on your needs)
-                        # pt_data += " " + str(current_time)
-                        # Print the data for verification
-                        print(pt_data)
-                        # Write data to CSV
-                        writeToCSV(pt_data, writer)
-                        split_pt_data = pt_data.split(",")
-                        pt_data = ",".join(split_pt_data[:-1])
-                        timestamp = int(split_pt_data[-1].split("=")[1]) * 1_000_000 + start_time_ns
-                        pt_data += " " + str(timestamp)
-                        print(pt_data)
-                        # Send data via UDP
-                        UDPClientSocket.sendto(pt_data.encode(), pressure_transducer_port)
-                        # Sleep for a short duration before reading the next value
-                        try:
-                            send_to_websocket(format_data_for_websockets(PUT SOMETHING HERE))
-                           except Exception as e:
-                            # Optionally log this
-                            print(f"WebSocket send failed: {e}")
-                            pass
-        def message_handling(client, userdata, msg):
-            parseInputAndGrafana(msg.payload.decode())
-        #client = paho.Client(client_id="unique_client_id", protocol=paho.MQTTv311)
-        client = paho.Client()
-        client.on_message = message_handling
-        if client.connect("localhost", 1883, 60) != 0:
-            print("Couldn't connect to the mqtt broker")
-            sys.exit(1)
-        client.subscribe("esp32/output")
-        try:
-            client.loop_forever()
-        except KeyboardInterrupt:
-            print(f"\nProgram terminated by user. Wrote data to {path}\n")
-            flag = False
+    # with open(path, mode='a', newline='') as file:  # Open the correct file here
+    #     writer = csv.writer(file)
+    #     # Write the header if the file is empty
+    #     if file.tell() == 0:
+    #         writer.writerow(['pt1', 'pt2', 'pt3', 'pt4', 'pt5', 'pt6', 'pt7', 'pt8', 'lc1', 'lc2', 'timestamp'])
+    #     def parseInputAndGrafana(input):
+    #         global flag
+    #         # Read the data and stream
+    #         if flag:
+    #             try:
+    #                 pt_data = input
+    #             except KeyboardInterrupt:
+    #                 # Handle KeyboardInterrupt to gracefully exit the program
+    #                 print(f"\nProgram terminated by user. Wrote data to {path}\n")
+    #                 flag = False
+    #             else:
+    #                 if pt_data.startswith('A') and pt_data.endswith('Z'):
+    #                     # Remove the 'A' at the start and 'Z' at the end
+    #                     pt_data = pt_data[1:-1]
+    #                     # Add the timestamp at the end (you can modify this based on your needs)
+    #                     # pt_data += " " + str(current_time)
+    #                     # Print the data for verification
+    #                     print(pt_data)
+    #                     forwebsockets = pt_data
+    #                     # Write data to CSV
+    #                     writeToCSV(pt_data, writer)
+    #                     split_pt_data = pt_data.split(",")
+    #                     pt_data = ",".join(split_pt_data[:-1])
+    #                     timestamp = int(split_pt_data[-1].split("=")[1]) * 1_000_000 + start_time_ns
+    #                     pt_data += " " + str(timestamp)
+    #                     print(pt_data)
+    #                     # Send data via UDP
+    #                     UDPClientSocket.sendto(pt_data.encode(), pressure_transducer_port)
+    #                     # Sleep for a short duration before reading the next value
+    #                     print(pt_data.encode())
+    #                     try:
+    #                         send_to_websocket(format_data_for_websockets(forwebsockets))
+    #                     except Exception as e:
+    #                         # Optionally log this
+    #                         print(f"WebSocket send failed: {e}")
+    #                         pass
+    #     def message_handling(client, userdata, msg):
+    #         parseInputAndGrafana(msg.payload.decode())
+    #     #client = paho.Client(client_id="unique_client_id", protocol=paho.MQTTv311)
+    #     client = paho.Client()
+    #     client.on_message = message_handling
+    #     if client.connect("localhost", 1883, 60) != 0:
+    #         print("Couldn't connect to the mqtt broker")
+    #         sys.exit(1)
+    #     client.subscribe("esp32/output")
+    #     try:
+    #         client.loop_forever()
+    #     except KeyboardInterrupt:
+    #         print(f"\nProgram terminated by user. Wrote data to {path}\n")
+    #         flag = False
 main()
 
 
